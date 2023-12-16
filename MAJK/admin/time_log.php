3abedfad9ef0a5f56a152d730e01e06d77f93df2
si-klyde <kclydebaclao@gmail.com>
2023-12-17 00:02:56 +0800

    atlog fix

--- MAJK/admin/time_log.php
+++ MAJK/admin/time_log.php
@@ -1,28 +1,57 @@
 <?php
 include 'db_connect.php';
-	extract($_POST);
-	$data= array();
-	$qry = $conn->query("SELECT * from employee where employee_id ='$eno' ");
-	if($qry->num_rows > 0){
-		$emp = $qry->fetch_array();
-		$save_log= $conn->query("INSERT INTO attendance (log_type,employee_id) values('$type','".$emp['employee_id']."')");
-		$employee = ucwords($emp['firstname'].'');
-		if($type == 1){
-			$log = ' time in this morning';
-		}elseif($type == 2){
-			$log = ' time out this morning';
-		}elseif($type == 3){
-			$log = ' time in this afternoon';
-		}elseif($type == 4){
-			$log = ' time out this afternoon';
-		}
-		if($save_log){
-				$data['status'] = 1;
-				$data['msg'] = $employee .', your '.$log.' has been sucessfully recorded. <br/>' ;
-			}
-		}else{	
-			$data['status'] = 2;
-			$data['msg'] = 'Failed! Unknown Employee Number';
-		}
-	echo json_encode($data);
-	$conn->close();
+extract($_POST);
+$data = array();
+
+// Check if the required fields are set
+if (!isset($eno, $type, $hour, $minute, $second, $period)) {
+    $data['status'] = 2;
+    $data['msg'] = 'Failed! Incomplete data.';
+    echo json_encode($data);
+    exit; // Terminate script execution
+}
+
+$currentDate = date('Y-m-d');
+
+// Query the database to check if the employee exists
+$qry = $conn->query("SELECT * from employee where employee_id ='$eno'");
+
+if ($qry->num_rows > 0) {
+    $emp = $qry->fetch_array();
+
+    // Construct the time in a format suitable for database insertion
+    $logTime = $hour . ':' . $minute . ':' . $second . ' ' . $period;
+
+    // Insert the log into the 'attendance' table
+
+	if ($type == 1) {
+		$save_log = $conn->query("INSERT IGNORE INTO attendance (atlog_date, employee_id, am_in) VALUES ('$currentDate', '{$emp['employee_id']}', '$logTime')");
+		$logMessage = ' time in this morning';
+	} elseif ($type == 2) {
+		$save_log = $conn->query("INSERT IGNORE INTO attendance (atlog_date, employee_id, am_out) VALUES ('$currentDate', '{$emp['employee_id']}', '$logTime')");
+		$logMessage = ' time out this morning';
+	} elseif ($type == 3) {
+		$save_log = $conn->query("INSERT IGNORE INTO attendance (atlog_date, employee_id, pm_in) VALUES ('$currentDate', '{$emp['employee_id']}', '$logTime')");
+		$logMessage = ' time in this afternoon';
+	} elseif ($type == 4) {
+		$save_log = $conn->query("INSERT IGNORE INTO attendance (atlog_date, employee_id, pm_out) VALUES ('$currentDate', '{$emp['employee_id']}', '$logTime')");
+		$logMessage = ' time out this afternoon';
+	}	
+
+    $employee = ucwords($emp['firstname']);
+
+    if ($save_log) {
+        $data['status'] = 1;
+        $data['msg'] = $employee . ', your ' . $logMessage . ' has been successfully recorded. <br/>';
+    } else {
+        $data['status'] = 2;
+        $data['msg'] = 'Failed to record time.';
+    }
+} else {
+    $data['status'] = 2;
+    $data['msg'] = 'Failed! Unknown Employee Number';
+}
+
+echo json_encode($data);
+$conn->close();
+?>
