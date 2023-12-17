d3e6de39e18874c3910c854aa08fc00382dc9a97
michaeljamil <michaeljamilmangga.cervano@bicol-u.edu.ph>
2023-12-17 19:50:41 +0800

    Updated Login Reports

--- MAJK/admin/login_reports.php
+++ MAJK/admin/login_reports.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <?php
-    require_once 'auth.php';
-    include 'db_connect.php'; // Include the file that establishes the database connection
+require_once 'auth.php';
+include 'db_connect.php'; // Include the file that establishes the database connection
 ?>
 <html lang="eng">
+
 <head>
     <title>Login Reports | Employee Attendance Record System</title>
     <?php include('header.php') ?>
@@ -38,6 +39,7 @@
         }
     </style>
 </head>
+
 <body>
     <?php include('nav_bar.php') ?>
     <div class="container-fluid admin">
@@ -52,52 +54,100 @@
             <div id="calendar"></div>
 
             <?php
-                $events = [];
-                $employeeColors = [
-                    1 => '#3498db', 
-                    2 => '#e74c3c', 
-                    3 => '#2ecc71',  
-                    4 => '#f39c12', 
-                    5 => '#9b59b6', 
-                ];
+            $events = [];
+            $employeeColors = [
+                1 => '#3498db',
+                2 => '#e74c3c',
+                3 => '#2ecc71',
+                4 => '#f39c12',
+                5 => '#9b59b6',
+            ];
 
-                $attendance_qry = $conn->query("SELECT a.*, CONCAT(e.firstname, ' ', e.middlename, ' ', e.lastname) AS name FROM `attendance` a INNER JOIN employee e ON a.employee_id = e.employee_id") or die(mysqli_error());
+            $attendance_qry = $conn->query("SELECT atlog_id, a.employee_id, CONCAT(e.firstname, ' ', e.middlename, ' ', e.lastname) AS name, atlog_date, am_in, am_out, pm_in, pm_out, am_late, am_undertime, pm_late, pm_undertime FROM `attendance` a INNER JOIN employee e ON a.employee_id = e.employee_id") or die(mysqli_error());
 
-                while ($row = $attendance_qry->fetch_array()) {
-                    $employeeId = $row['employee_id'];
+            while ($row = $attendance_qry->fetch_array()) {
+                $employeeId = $row['employee_id'];
 
-                    if (!isset($employeeColors[$employeeId])) {
-                        $employeeColors[$employeeId] = '#95a5a6'; 
-                    }
-
-                    // Create "time in" event
-                    $eventIn = [
-                        'title' => $row['name'] . ' (Time In)',
-                        'start' => $row['atlog_date'] . ' ' . $row['am_in'], // Adjust this according to your database structure
-                        'allDay' => false,
-                        'url' => 'javascript:void(0);',
-                        'color' => $employeeColors[$employeeId], 
-                    ];
-
-                    // Create "time out" event
-                    $eventOut = [
-                        'title' => $row['name'] . ' (Time Out)',
-                        'start' => $row['atlog_date'] . ' ' . $row['pm_out'], // Adjust this according to your database structure
-                        'allDay' => false,
-                        'url' => 'javascript:void(0);',
-                        'color' => $employeeColors[$employeeId], 
-                    ];
-
-                    array_push($events, $eventIn, $eventOut);
+                if (!isset($employeeColors[$employeeId])) {
+                    $employeeColors[$employeeId] = '#95a5a6';
                 }
+
+                // Check if "AM Time In" is not null before creating the event
+                if (isset($row['am_in'])) {
+                    // Create "AM Time In" event
+                    $eventInAM = [
+                        'title' => $row['name'] . ' (AM Time In) - ' . getStatus($row['am_late'], $row['am_undertime']) . ' - ' . $row['am_in'],
+                        'start' => $row['atlog_date'] . 'T' . $row['am_in'], // ISO 8601 date-time format
+                        'allDay' => false,
+                        'color' => $employeeColors[$employeeId],
+                    ];
+
+                    // Add "AM Time In" event to the $events array
+                    array_push($events, $eventInAM);
+                }
+
+                // Check if "AM Time Out" is not null before creating the event
+                if (isset($row['am_out'])) {
+                    // Create "AM Time Out" event
+                    $eventOutAM = [
+                        'title' => $row['name'] . ' (AM Time Out) - ' . getStatus($row['am_late'], $row['am_undertime']) . ' - ' . $row['am_out'],
+                        'start' => $row['atlog_date'] . 'T' . $row['am_out'], // ISO 8601 date-time format
+                        'allDay' => false,
+                        'color' => $employeeColors[$employeeId],
+                    ];
+
+                    // Add "AM Time Out" event to the $events array
+                    array_push($events, $eventOutAM);
+                }
+
+                // Check if "PM Time In" is not null before creating the event
+                if (isset($row['pm_in'])) {
+                    // Create "PM Time In" event
+                    $eventInPM = [
+                        'title' => $row['name'] . ' (PM Time In) - ' . getStatus($row['pm_late'], $row['pm_undertime']) . ' - ' . $row['pm_in'],
+                        'start' => $row['atlog_date'] . 'T' . $row['pm_in'], // ISO 8601 date-time format
+                        'allDay' => false,
+                        'color' => $employeeColors[$employeeId],
+                    ];
+
+                    // Add "PM Time In" event to the $events array
+                    array_push($events, $eventInPM);
+                }
+
+                // Check if "PM Time Out" is not null before creating the event
+                if (isset($row['pm_out'])) {
+                    // Create "PM Time Out" event
+                    $eventOutPM = [
+                        'title' => $row['name'] . ' (PM Time Out) - ' . getStatus($row['pm_late'], $row['pm_undertime']) . ' - ' . $row['pm_out'],
+                        'start' => $row['atlog_date'] . 'T' . $row['pm_out'], // ISO 8601 date-time format
+                        'allDay' => false,
+                        'color' => $employeeColors[$employeeId],
+                    ];
+
+                    // Add "PM Time Out" event to the $events array
+                    array_push($events, $eventOutPM);
+                }
+            }
+
+            // Function to determine late or undertime status
+            function getStatus($late, $undertime) {
+                if ($late === 'Late') {
+                    return 'Late';
+                } elseif ($undertime === 'Undertime') {
+                    return 'Undertime';
+                } else {
+                    return 'On Time';
+                }
+            }
             ?>
         </div>
     </div>
 </body>
+
 <script type="text/javascript">
     $(document).ready(function () {
         var events = <?php echo json_encode($events); ?>;
-        var filteredEvents = events.slice(); 
+        var filteredEvents = events.slice();
 
         $('#calendar').fullCalendar({
             header: {
@@ -109,28 +159,29 @@
             navLinks: true,
             editable: false,
             eventLimit: true,
-            displayEventTime: false, 
-            displayEventEnd: false, 
-            allDaySlot: false,     
+            displayEventTime: false,
+            displayEventEnd: false,
+            allDaySlot: false,
             events: events,
-            eventClick: function(calEvent, jsEvent, view) {
+            eventClick: function (calEvent, jsEvent, view) {
                 alert('Attendance: ' + calEvent.title);
             },
-            viewRender: function(view, element) {
+            viewRender: function (view, element) {
                 $('.fc-axis.fc-widget-header').hide();
             }
         });
 
-        $('#searchInput').on('input', function() {
+        $('#searchInput').on('input', function () {
             var searchString = $(this).val().toLowerCase();
-            
-            filteredEvents = events.filter(function(event) {
+
+            filteredEvents = events.filter(function (event) {
                 return event.title.toLowerCase().includes(searchString);
             });
-            
+
             $('#calendar').fullCalendar('removeEvents');
             $('#calendar').fullCalendar('addEventSource', filteredEvents);
         });
     });
 </script>
+
 </html>
